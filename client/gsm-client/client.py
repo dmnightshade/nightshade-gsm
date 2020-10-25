85283d492ce461396d70905392bb2bf27b285be2
Mike Umana <mu304007@gmail.com>
2020-10-24 18:12:17 -0700

    Made client to ask server for game list and present to user

---
+++ client/gsm-client/client.py
@@ -0,0 +1,127 @@
+from threading import Thread
+import json
+from qtpy.QtWidgets import *
+from qtpy.QtNetwork import QTcpSocket
+from qtpy.QtCore import *
+from time import sleep, perf_counter
+from typing import Dict
+
+
+class Client(QMainWindow):
+    def __init__(self):
+        super().__init__(parent=None)
+
+        self.socket = QTcpSocket(self)
+
+        #### Set central widget, which is what is shown on the GUI ####
+        main_widget = QWidget(self)
+        self.layout = QStackedLayout(main_widget)
+        self.setCentralWidget(main_widget)
+
+        #### Not connected page ####
+        not_connected = QPushButton(text="Connect to server", parent=main_widget)
+        not_connected.clicked.connect(self.connect_to_server)
+        self.layout.addWidget(not_connected)
+        self.socket.disconnected.connect(lambda: self.change_page(0))
+
+        #### Connected page ####
+        connected = QWidget(parent=main_widget)
+        c_layout = QVBoxLayout(connected)
+        self.layout.addWidget(connected)
+        self.socket.connected.connect(self.populate_game_list)
+
+        # Disconnect button #
+        disconnect_button = QPushButton(text="Disconnect from server", parent=connected)
+        disconnect_button.clicked.connect(self.disconnect_from_server)
+        disconnect_button.setMaximumHeight(30)
+        c_layout.addWidget(disconnect_button)
+
+        # Game selection area #
+        self.game_area = QHBoxLayout(connected)
+        c_layout.addLayout(self.game_area)
+
+        # Lists available game server to create
+        self.game_list = None
+
+        # Holds the server creations dialogs
+        # This is set by self.populate_game_list()
+        self.game_creation = None
+
+    def change_page(self, page_num: int) -> None:
+        self.layout.setCurrentIndex(page_num)
+
+    def connect_to_server(self) -> None:
+        self.socket.connectToHost("127.0.0.1", 12345)
+
+    def disconnect_from_server(self) -> None:
+        self.socket.disconnectFromHost()
+
+    def fetch_game_list(self) -> Dict:
+        cmd = QByteArray("gamelist\n".encode(encoding="utf-8"))
+        self.socket.write(cmd)
+        self.socket.waitForReadyRead(msecs=3000)
+        game_list = str(self.socket.readLine(), encoding="utf-8")
+        game_list = json.loads(game_list)
+        print(game_list)
+        return game_list
+
+    def populate_game_list(self) -> None:
+        # Ask server for game list
+        gl = self.fetch_game_list()
+
+        # Remove previous game list
+        if self.game_list is not None:
+            self.game_area.layout().removeWidget(self.game_list)
+            self.game_list = None
+
+        # Remove any previous creation area
+        if self.game_creation is not None:
+            self.game_area.layout().removeWidget(self.game_creation)
+            self.game_creation = None
+
+        # Create new game list
+        self.game_list = QListWidget()
+        self.game_list.setMaximumWidth(200)
+        self.game_area.addWidget(self.game_list)
+
+        # Create new game creation area
+        self.game_creation = QWidget()
+        c_layout = QStackedLayout(self.game_creation)
+        self.game_area.addWidget(self.game_creation)
+
+        # Now add the games in the game list
+        for name in sorted(gl.keys()):
+            self.game_list.addItem(name)
+            info = gl[name]
+            form_w = QWidget(self.game_creation)
+
+            form = QFormLayout(form_w)
+            field = QTextEdit(info["exec"], form_w)
+            field.setEnabled(False)
+            form.addRow("Program:", field)
+            for arg_name, arg_info in info["args"].items():
+                field = QTextEdit(str(arg_info["default"]), form_w)
+                if "editable" in arg_info:
+                    field.setEnabled(arg_info["editable"])
+                else:
+                    field.setEnabled(True)
+                form.addRow(arg_name, field)
+            c_layout.addWidget(form_w)
+
+        # Set starting selection
+        self.game_list.setCurrentRow(0)
+        c_layout.setCurrentIndex(0)
+
+        # Creation area changes with game list selections
+        self.game_list.currentRowChanged.connect(c_layout.setCurrentIndex)
+
+        # Show game list page
+        self.change_page(1)
+
+
+if __name__ == '__main__':
+    # Start client
+    app = QApplication([])
+    client = Client()
+    client.show()
+    app.exec_()
---
+++ client/gsm-server/server.py
@@ -0,0 +1,50 @@
+from socketserver import TCPServer, StreamRequestHandler
+import json
+
+
+class Handler(StreamRequestHandler):
+    games = {
+        "game2": {
+            "exec": "program1",
+            "args": {
+                "-a1": {
+                    "default": 100,
+                    "editable": False
+                },
+                "-a2": {
+                    "default": 200
+                }
+            }
+            },
+        "game1": {
+            "exec": "program2",
+            "args": {
+                "-a3": {
+                    "default": 300
+                }
+            }
+        }
+    }
+
+    def handle(self) -> None:
+        print(f"Connection from: {self.client_address}")
+        while True:
+            raw = self.rfile.readline()
+            command = raw.decode(encoding="utf-8").strip().lower()
+            print(f"{self.client_address[0]} wrote:\n{command}")
+            if command == "gamelist":
+                print(f"Sending:\n{json.dumps(self.games)}")
+                gamelist = json.dumps(self.games) + "\n"
+                self.wfile.write(gamelist.encode("utf-8"))
+            elif command == "":
+                print(f"Disconnecting from: {self.client_address}")
+                break
+
+
+def start_server() -> None:
+    with TCPServer(("127.0.0.1", 12345), Handler) as server:
+        server.serve_forever()
+
+
+if __name__ == '__main__':
+    start_server()
